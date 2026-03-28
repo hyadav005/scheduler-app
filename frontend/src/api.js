7107b5aef2996d331fca52376628096cfd98b98b
Himanshu Yadav <152169545+hyadav005@users.noreply.github.com>
2026-03-29 03:53:45 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://scheduler-app-2-jrzt.onrender.com",
 });
 
 export const apiService = {
